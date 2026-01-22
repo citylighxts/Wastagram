2dfaa13b53a70da45afdd91c86be149f7f034500
citylighxts <hanazizahn@gmail.com>
2026-01-22 14:56:12 +0700

    Initial Commit

---
+++ Wastagram/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Wastagram
+//
+//  Created by Hana Azizah on 22/01/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ WastagramTests/WastagramTests.swift
@@ -0,0 +1,17 @@
+//
+//  WastagramTests.swift
+//  WastagramTests
+//
+//  Created by Hana Azizah on 22/01/26.
+//
+
+import Testing
+@testable import Wastagram
+
+struct WastagramTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
