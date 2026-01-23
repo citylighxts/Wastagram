b569b2e4b7128b3c38918aba0935839ffc2702b0
Rayhan Aurelia <rayhanaurelia585@gmail.com>
2026-01-23 10:20:29 +0700

    Suggestion-Demo

--- Wastagram/Models/SetorModels.swift
+++ Wastagram/Models/SetorModels.swift
@@ -1,4 +1,5 @@
 import SwiftUI
+import CoreLocation
 
 enum WasteType { case anorganic, organic }
 enum CompostMethod { case returnBack, donate }
@@ -6,4 +7,27 @@ enum ScheduleType { case now, scheduled, subscribe }
 
 struct Style {
     static let dash = StrokeStyle(lineWidth: 1, dash: [5])
+}
+
+// MARK: - Order Models
+struct Order: Identifiable {
+    let id: String
+    let location: CLLocationCoordinate2D
+    let address: String
+    let weight: Double // dalam kg
+    let wasteType: WasteType
+    let timestamp: Date
+    let customerName: String
+}
+
+// MARK: - Batch Suggestion
+struct BatchSuggestion: Identifiable {
+    let id = UUID()
+    let orders: [Order]
+    let totalWeight: Double
+    let direction: String // "Utara", "Selatan", dll.
+    
+    var orderCount: Int {
+        orders.count
+    }
 }
